1f333e3aee0961a220226893b47eef626a5deb57
Jonathan Gillespie <148815427+jgillespie-assurity@users.noreply.github.com>
2024-04-03 14:58:41 +1300

    Copilot generated gherkin tests and step implementation for API /Account/v1/Authorized

--- src/test/java/org/example/ApiTestSteps.java
+++ src/test/java/org/example/ApiTestSteps.java
@@ -2,21 +2,48 @@ package org.example;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ApiTestSteps {
+    private final String rootUrl = "https://demoqa.com";
     private Response response;
+    private RequestSpecification request;
 
-    @Given("I make a GET request to {string}")
-    public void iMakeAGETRequestTo(String url) {
-        response = RestAssured.get(url);
+    @Given("I am authorized")
+    public void iAmAuthorized() {
+        request = RestAssured.given().auth().preemptive().basic("username", "password");
+    }
+
+    @Given("I am not authorized")
+    public void iAmNotAuthorized() {
+        request = RestAssured.given();
+    }
+
+    @Given("I have an invalid body for the request")
+    public void iHaveAnInvalidBodyForTheRequest() {
+        request.body("invalid body");
+    }
+
+    @When("I make a POST request to {string}")
+    public void iMakeAPostRequestTo(String url) {
+        response = request.post(rootUrl + url);
     }
 
     @Then("the response status code should be {int}")
     public void theResponseStatusCodeShouldBe(int statusCode) {
         assertEquals(statusCode, response.getStatusCode());
+    }
+
+    @Then("the response body should contain the expected data")
+    public void theResponseBodyShouldContainTheExpectedData() {
+        // This is a placeholder. You should replace this with the actual assertion
+        // that checks the response body contains the expected data.
+        assertTrue(response.getBody().asString().contains("expected data"));
     }
 }
--- src/test/resources/api_test.feature
+++ src/test/resources/api_test.feature
@@ -1,5 +1,18 @@
-Feature: API testing with RestAssured
+Feature: Testing of DemoQA Bookstore API
 
-  Scenario: Successful GET request
-    Given I make a GET request to "https://demoqa.com/BookStore/v1/Books"
+  Scenario: Successful POST request to AccountV1AuthorizedPost
+    Given I am authorized
+    When I make a POST request to "/Account/v1/Authorized"
     Then the response status code should be 200
+    And the response body should contain the expected data
+
+  Scenario: Unsuccessful POST request to AccountV1AuthorizedPost with unauthorized user
+    Given I am not authorized
+    When I make a POST request to "/Account/v1/Authorized"
+    Then the response status code should be 401
+
+Scenario: Bad POST request to AccountV1AuthorizedPost with invalid body
+  Given I am authorized
+  And I have an invalid body for the request
+  When I make a POST request to "/Account/v1/Authorized"
+  Then the response status code should be 400
